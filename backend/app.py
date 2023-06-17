d1bdc140ee0f863a0e9a95f540432bc6551f7bc8
naz2001r <naz2001r@gmail.com>
2023-06-17 21:44:54 +0300

    Added fastapi app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,22 @@
-import os
-
+from src.models import *
 from fastapi import FastAPI
+from src.routers.chatbot import chatbot_router
 
-app = FastAPI()
 
+app = FastAPI(
+    title="PDF Chatbot App API",
+    description="""FastAPI documentation for PDF Chatbot App""",
+    contact ={
+        'name': 'Demo App',
+        'url': 'http://localhost:8501'
+    },
+    version="0.1.0",
+)
+app.include_router(chatbot_router)
 
-@app.get("/example")
-async def example():
-    #print(os.environ['TEST_SECRET']) # now not in env var
-    return {"message": 'Hello'}
+tags_metadata = [
+    {
+        'name': 'PDF Chatbot',
+        'descriptions': ' PDF Chatbot for answering to the questions, which releated to PDF files.'
+    }
+]
