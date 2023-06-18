2b63834da7d1b82f856b38ebb46167171d0d927b
naz2001r <naz2001r@gmail.com>
2023-06-18 10:55:29 +0300

    Added QueryObject model

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-
-file_name = "file.pdf"
-db_name = "vector_db.pkl"
+try:
+    from src.constants import file_name, db_name
+except:
+    from backend.src.constants import file_name, db_name
 
 class OpenAPIBaseModel(BaseModel):
     openai_key: str
@@ -9,3 +10,9 @@ class OpenAPIBaseModel(BaseModel):
 class PDFObject(OpenAPIBaseModel):
     file_name: str = file_name
     db_name: str = db_name
+
+class QueryObject(OpenAPIBaseModel):
+    db_path: str
+    k: int = 3 
+    question: str
+    return_only_outputs: bool = True
